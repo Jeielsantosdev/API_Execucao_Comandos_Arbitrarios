e938765e2e515b723a30436efb797fd3db1ee874
jeiel <jeielsantos29@gmail.com>
2025-06-10 13:32:24 -0300

    add: fixed pagination error

--- api/pagination.py
+++ api/pagination.py
@@ -5,15 +5,18 @@ class SafePagination(LimitOffsetPagination):
     max_limit = 1000 
     max_offset = 10000 
     def paginate_queryset(self, queryset, request, view=None, **kwargs):
-        if self.limit is not None:
-            if self.limit <= 0:
+        limit = self.get_limit(request)
+        offset = self.get_offset(request)
+
+        if limit is not None:
+            if limit <= 0:
                 raise HttpError(400, "max_limit must be greater than 0")
-            if self.limit > self.max_limit:
-                self.limit = self.max_limit
-        if self.offset is not None:
-            if self.offset < 0:
+            if limit > self.max_limit:
+                limit = self.max_limit
+        if offset is not None:
+            if offset < 0:
                 raise HttpError(400, "offset must be greater than or equal to 0")
-            if self.offset > self.max_offset:
+            if offset > self.max_offset:
                 raise HttpError(400, "offset must be less than or equal to max_offset")
         return super().paginate_queryset(queryset, request, view, **kwargs)
                
