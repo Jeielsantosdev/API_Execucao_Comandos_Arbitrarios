b62e199de80185512d5d3ac8b97ea61d7a451e6e
jeiel <jeielsantos29@gmail.com>
2025-06-10 13:11:36 -0300

    add: fixed pagination error

--- api/pagination.py
+++ api/pagination.py
@@ -5,7 +5,7 @@ class SafePagination(LimitOffsetPagination):
     Custom pagination class that extends LimitOffsetPagination
     to ensure safe pagination behavior.
     """
-    def paginate_queryset(self, queryset, request, view=None):
+    def paginate_queryset(self, queryset, request, view=None, **kwargs):
         # Ensure that the limit is not too high
         if self.limit is not None and self.limit > 1000:
             self.limit = 1000
